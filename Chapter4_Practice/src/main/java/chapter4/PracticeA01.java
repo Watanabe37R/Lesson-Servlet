1655249a13d2611d42e8d9d3f594b3875708fa72
Watanabe37R <ryota.watanabe@arsnv.co.jp>
2026-05-15 16:20:36 +0900

    Chapter4演習完了

---
+++ Chapter4_Practice/src/main/java/chapter4/PracticeA01.java
@@ -0,0 +1,40 @@
+package chapter4;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PracticeA01
+ */
+@WebServlet("/chapter4/practicea01")
+public class PracticeA01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/plain; charset=UTF-8");
+		response.getWriter().println("GETでアクセスされました");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		response.setContentType("text/plain; charset=UTF-8");
+		response.getWriter().println("POSTでアクセスされました");
+		
+	}
+
+}
